6a59a52eadbbe04210961e831a0cab36939c45c5
Alan Daniel Alvarez Castro <alandanielalvarez0000@gmail.com>
2025-11-13 02:01:27 -0600

    wip

--- apps/contracts/simulated-output.js
+++ apps/contracts/simulated-output.js
@@ -1,4 +1,4 @@
-// Simulated deployment output for reference
+// Simulated deployment output for reference:
 console.log("Simulated deployment output:");
 console.log("CrosswordBoard deployed to: 0x5fbdb2315678afecb367f032d93f642f64180aa3");
 console.log("CrosswordPrizes deployed to: 0xe7f1725e7734ce288f8367e1bb143e90bb3f0512");
